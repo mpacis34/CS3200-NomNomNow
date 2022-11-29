f61ada3228ced40c72f8ebd65c8d656234b24ded
Tri Watanasuparp <triwatanasuparp@Tris-MacBook-Pro.local>
2022-11-29 17:39:24 -0500

    revertback to template

--- flask-app/app.py
+++ flask-app/app.py
@@ -6,20 +6,8 @@
 # that lives in src/__init__.py
 from src import create_app
 
-from flask import Flask, jsonify
-from flaskext.mysql import MySQL
-
 # create the app object
 app = create_app()
-
-app.config['MYSQL_DATABASE_HOST'] = 'db'
-app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_USER'] = 'nomnomnowAdmin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'bnD50!9OH2f'
-app.config['MYSQL_DATABASE_DB'] = 'nomnomnow_db'
-
-db_connection = MySQL()
-db_connection.init_app(app)
 
 if __name__ == '__main__':
     # we want to run in debug mode (for hot reloading) 
