ddae58883f240ddc1742c53c31b944c91f563d13
Tri Watanasuparp <triwatanasuparp@Tris-MacBook-Pro.local>
2022-12-08 13:17:19 -0500

    connect to widgets

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -21,6 +21,33 @@ def get_customers():
     the_response.mimetype = 'application/json'
     return the_response
 
+# Get all the restaurants from the database
+# this is our first route
+@customers.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute('select rest_id, rest_name, info, opening_hour, closing_hour from Restaurant')
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
 # Get customer detail for customer with particular userID
 @customers.route('/customers/<userID>', methods=['GET'])
 def get_customer(userID):
@@ -35,3 +62,30 @@ def get_customer(userID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+    
+    # Get all the menu items given a restaurantID
+# this is our 2nd route
+@customers.route('/restaurant_menu/<rest_id>', methods=['GET'])
+def get_restaurant_menuItems(rest_id):
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute('select mi.item_name, mi.item_description, mi.price, mi.availability FROM MenuItem mi join Menu m on m.menu_id = mi.menu_id where m.rest_id={0}'.format(rest_id))    
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
--- flask-app/src/restaurants/restaurants.py
+++ flask-app/src/restaurants/restaurants.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response, app
+from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
@@ -13,7 +13,7 @@ def get_restaurants():
     cursor = db.get_db().cursor()
 
     # use cursor to query the database for a list of products
-    cursor.execute('select rest_name from Restaurant')
+    cursor.execute('select rest_name, info, opening_hour, closing_hour from Restaurant')
 
     # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
@@ -90,38 +90,22 @@ def get_restaurant_getAllRestOrders(rest_id):
 # add a new menu item to a menu of a specific restaurant
 # this is our 4th route
 @restaurants.route('/restaurants/addMenuItem', methods=['POST'])
-def get_restaurant_addNewMenuItem():
+def addNewMenuItem():
     
-    app.logger.info(request.form);
+    current_app.logger.info(request.form);
     cursor = db.get_db().cursor();
     menu_id = request.form['menu_id']
     food_category_id = request.form['food_category_id']
     name = request.form['name']
     description = request.form['description']
     price = request.form['price']
-    availability = request.form['availability']
-    query = f'INSERT INTO MenuItem (menu_id, food_category_id, item_name, item_description, price, item_availability) VALUES (\"{menu_id}"\, \"{food_category_id}"\, \"{name}"\,\"{description}"\,\"{price}\",\"{availability}"\)'
 
+    query = f'INSERT INTO MenuItem (menu_id, food_category_id, item_name, item_description, price, item_availability) VALUES ({menu_id}, {food_category_id}, "{name}", "{description}", {price}, true)'
+    current_app.logger.info(query);
 
-    # use cursor to query the database for a list of products
     cursor.execute(query)
-
-    # grab the column headers from the returned data
-    column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
-    json_data = []
-
-    # fetch all the data from the cursor
-    theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
-    for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
-
-    return jsonify(json_data)
+    db.get_db().commit()
+    return 'Success!'
 
 # # get the top 5 products from the database
 # @restaurants.route('/top5products')
