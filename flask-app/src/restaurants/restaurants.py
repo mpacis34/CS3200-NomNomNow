bcd5b24cf5b90606cb1d92b7f71bb57d2b9dbf06
Tri Watanasuparp <triwatanasuparp@Tris-MBP.lan1>
2022-12-13 14:41:54 -0500

    cleaned up routes

--- flask-app/src/restaurants/restaurants.py
+++ flask-app/src/restaurants/restaurants.py
@@ -5,10 +5,37 @@ from src import db
 
 restaurants = Blueprint('restaurants', __name__)
 
-# Get all the the orders for a given restaurant
+# Get all the restaurants from the database
+# this is our first route
+@restaurants.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute('select rest_id, rest_name, info, opening_hour, closing_hour from Restaurant')
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+# Get all the orders for a given restaurant
 # this is our 3rd route
 @restaurants.route('/restaurants/orders/<rest_id>', methods=['GET'])
-def get_restaurant_getAllRestOrders(rest_id):
+def get_all_restaurant_orders(rest_id):
     # get a cursor object from the database
     cursor = db.get_db().cursor()
 
